93f7686150f4008bfafcef64e24a670c0a08e688
Nepomuceno <149882667+penepo@users.noreply.github.com>
2024-05-09 12:57:59 -0300

    Update Passagem.java

--- Passagem.java
+++ Passagem.java
@@ -1,26 +1,22 @@
-import java.util.Date;
+import java.util.Scanner;
 
 public class Passagem {
-	String classe;
-	double preco;
-	String assento;
-	float horario;
-	String destino;
-	String portao;
-	String voo;
-	String nome;
-	String origem;
 	
-	public Passagem (String classe, double preco, String assento, float horario, String destino, String portao, String voo, 
-			String nome, String origem) {
-		this.classe = classe;
-		this.preco = preco;
-		this.horario = horario;
-		this.assento = assento;
-		this.destino = destino;
-		this.origem = origem;
-		this.voo = voo;
-		this.portao = portao;
+	Scanner sc = new Scanner(System.in);
+	
+	public String nome;
+	public String classe;
+	public double preco;
+	public String assento;
+	public String horario;
+	public String origem;
+	public String destino;
+	public String portao;
+	public String voo;
+	
+	
+	public void criarPassagem() {
 		
 	}
+	
 }
